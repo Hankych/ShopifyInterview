57fcf11bb1e8a4cd836d7261a35a112551b953f9
Hankych <hankm711sr@gmail.com>
2025-01-31 13:52:00 -0500

    Skeleton

---
+++ WordGuess.py
@@ -0,0 +1,27 @@
+
+class WordGuess:
+    def __init__(self, valid_words, target_word):
+        self.valid_words = valid_words
+        self.target_word = target_word
+        
+    def create_hint(self, guess):
+        
+    
+def play_word_game():
+    word_list = {"able", "bell", "boss", "cast", "cash", "knot", "note", "near", "over", "salt", "wood"}
+    
+    # game start
+    
+    # first turn
+    
+    # Take in user guess
+    
+    # create hint
+    
+    # round incremnt
+    
+    # win or lose (5 rounds)
+    
+    
+if __name__ == "__main__":
+    play_word_game()
